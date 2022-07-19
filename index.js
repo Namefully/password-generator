a769b41ff9e6a79b491255349b6c9154313b0c35
Namefully <108553244+Namefully@users.noreply.github.com>
2022-07-19 18:49:56 +0300

    Update index.js

---
+++ index.js
@@ -0,0 +1,129 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?", "/"];
+ let pass1 = document.getElementById("pass1")
+ let pass2 = document.getElementById("pass2")    
+ function generate(){   
+    let gen1 = Math.floor(Math.random() * characters.length)
+     pass1.innerText = characters[gen1]
+     
+    let gen2 = Math.floor(Math.random() * characters.length)
+     pass1.innerText += characters[gen2]
+     
+         let gen3 = Math.floor(Math.random() * characters.length)
+     pass1.innerText += characters[gen3]
+     
+         let gen4 = Math.floor(Math.random() * characters.length)
+     pass1.innerText += characters[gen4]
+     
+         let gen5 = Math.floor(Math.random() * characters.length)
+     pass1.innerText += characters[gen5]
+     
+         let gen6 = Math.floor(Math.random() * characters.length)
+     pass1.innerText += characters[gen6]
+     
+         let gen7 = Math.floor(Math.random() * characters.length)
+     pass1.innerText += characters[gen7]
+     
+         let gen8 = Math.floor(Math.random() * characters.length)
+     pass1.innerText += characters[gen8]
+     
+         let gen9 = Math.floor(Math.random() * characters.length)
+     pass1.innerText += characters[gen9]
+     
+         let gen10 = Math.floor(Math.random() * characters.length)
+     pass1.innerText += characters[gen10]
+     
+         let gen11 = Math.floor(Math.random() * characters.length)
+     pass1.innerText += characters[gen11]
+     
+         let gen12 = Math.floor(Math.random() * characters.length)
+     pass1.innerText += characters[gen12]
+     
+         let gen13 = Math.floor(Math.random() * characters.length)
+     pass1.innerText += characters[gen13]
+     
+         let gen14 = Math.floor(Math.random() * characters.length)
+     pass1.innerText += characters[gen14]
+     
+         let gen15 = Math.floor(Math.random() * characters.length)
+     pass1.innerText += characters[gen15]
+     
+         let gen16 = Math.floor(Math.random() * characters.length)
+     pass1.innerText += characters[gen16]
+     
+         let gen17 = Math.floor(Math.random() * characters.length)
+     pass1.innerText += characters[gen17]
+     
+         let gen18 = Math.floor(Math.random() * characters.length)
+     pass1.innerText += characters[gen18]
+     
+         let gen19 = Math.floor(Math.random() * characters.length)
+     pass1.innerText += characters[gen19]
+     
+              let gen20 = Math.floor(Math.random() * characters.length)
+     pass1.innerText += characters[gen20]
+     
+     
+    
+    
+      
+    let gen1b = Math.floor(Math.random() * characters.length)
+     pass2.innerText = characters[gen1b]
+     
+    let gen2b = Math.floor(Math.random() * characters.length)
+     pass2.innerText += characters[gen2b]
+     
+         let gen3b = Math.floor(Math.random() * characters.length)
+     pass2.innerText += characters[gen3b]
+     
+         let gen4b = Math.floor(Math.random() * characters.length)
+     pass2.innerText += characters[gen4b]
+     
+         let gen5b = Math.floor(Math.random() * characters.length)
+     pass2.innerText += characters[gen5b]
+     
+         let gen6b = Math.floor(Math.random() * characters.length)
+     pass2.innerText += characters[gen6b]
+     
+         let gen7b = Math.floor(Math.random() * characters.length)
+     pass2.innerText += characters[gen7b]
+     
+         let gen8b = Math.floor(Math.random() * characters.length)
+     pass2.innerText += characters[gen8b]
+     
+         let gen9b = Math.floor(Math.random() * characters.length)
+     pass2.innerText += characters[gen9b]
+     
+         let gen10b = Math.floor(Math.random() * characters.length)
+     pass2.innerText += characters[gen10b]
+     
+         let gen11b = Math.floor(Math.random() * characters.length)
+     pass2.innerText += characters[gen11b]
+     
+         let gen12b = Math.floor(Math.random() * characters.length)
+     pass2.innerText += characters[gen12b]
+     
+         let gen13b = Math.floor(Math.random() * characters.length)
+     pass2.innerText += characters[gen13b]
+     
+         let gen14b = Math.floor(Math.random() * characters.length)
+     pass2.innerText += characters[gen14b]
+     
+         let gen15b = Math.floor(Math.random() * characters.length)
+     pass2.innerText += characters[gen15b]
+     
+         let gen16b = Math.floor(Math.random() * characters.length)
+     pass2.innerText += characters[gen16b]
+     
+         let gen17b = Math.floor(Math.random() * characters.length)
+     pass2.innerText += characters[gen17b]
+     
+         let gen18b = Math.floor(Math.random() * characters.length)
+     pass2.innerText += characters[gen18b]
+     
+         let gen19b = Math.floor(Math.random() * characters.length)
+     pass2.innerText += characters[gen19b]
+     
+              let gen20b = Math.floor(Math.random() * characters.length)
+     pass2.innerText += characters[gen20b]}
+     
+   
